8f9131817559183782f6eef18e7f961be8db3cd2
MaxPirotaisWilton <maxpwhirunda@gmail.com>
2022-07-23 17:29:03 +1200

    Player Movement Complete
    Player movement has now been fully implemented, both for individual players, but also when merged.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,26 +8,55 @@ public class Player : MonoBehaviour
     public int playerNum = 1;
 
     private Rigidbody rBody;
+    private PlayerManager pManager;
+    private bool attracting = false;
+    private bool merged = false;
+    private float movementPenalty = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody>();
+        pManager = FindObjectOfType<PlayerManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Button_P" + playerNum.ToString()))
+        {
+            pManager.buttonPressed();
+        }
+        else if(Input.GetButtonUp("Button_P" + playerNum.ToString()))
+        {
+            pManager.buttonReleased();
+        }
+        
         Vector2 inputVector = new Vector2(Input.GetAxis("Hor_P" + playerNum.ToString()), Input.GetAxis("Ver_P" + playerNum.ToString()));
-        Debug.Log("Input Debug: Movement_Vector = " + inputVector.ToString() + " Button = " + Input.GetButton("Button_P" + playerNum.ToString()));
-
-
-        rBody.AddForce(10 * (Vector3) inputVector - rBody.velocity);
+        //Debug.Log("Input Debug: Movement_Vector = " + inputVector.ToString() + " Button = " + Input.GetButton("Button_P" + playerNum.ToString()));
+        rBody.AddForce((10 * (Vector3)inputVector - rBody.velocity) * movementPenalty);
+        
 
     }
 
     public Rigidbody getRigidbody()
     {
         return rBody;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (attracting)
+        {
+            pManager.Merge(this.gameObject);
+            attracting = false;
+        }
+    }
+
+    public void Attract(Transform pTransform)
+    {
+        attracting = true;
+        Vector3 toPlayer = (pTransform.position - transform.position).normalized;
+        rBody.AddForce(40 * toPlayer - rBody.velocity/2);
     }
 }
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,13 +6,25 @@ public class PlayerManager : MonoBehaviour
 {
     public GameObject debugMiddle;
 
+    private bool playersCombined = false;
+    private bool playersAttracting = false;
     private Player[] players;
+    private Rigidbody[] rBodies;
     private Vector3 playerMiddle;
+    private FixedJoint joint;
+
+    private int buttonDualHold = 0;
+    private bool dualButtonSuccess = false;
 
     // Start is called before the first frame update
     void Start()
     {
         players = FindObjectsOfType<Player>();
+        rBodies = new Rigidbody[players.Length];
+        for(int i = 0; i < rBodies.Length; i++)
+        {
+            rBodies[i] = players[i].GetComponent<Rigidbody>();
+        }
     }
 
     // Update is called once per frame
@@ -20,5 +32,68 @@ public class PlayerManager : MonoBehaviour
     {
         playerMiddle = (players[0].transform.position + players[1].transform.position) / 2;
         debugMiddle.transform.position = playerMiddle;
+
+        Debug.Log(dualButtonSuccess);
+
+        if(buttonDualHold == 2 && !dualButtonSuccess)
+        {
+            Debug.Log("DualButton Action");
+            dualButtonSuccess = true;
+
+            if (!playersCombined)
+            {
+                Debug.Log("Merge");
+                playersAttracting = true;
+            }
+            else
+            {
+                if(joint != null)
+                {
+                   
+                    Vector3 p1top2 = (players[1].transform.position - players[0].transform.position).normalized;
+                    Destroy(joint);
+                    rBodies[0].velocity = 5 * p1top2;
+                    rBodies[1].velocity = -5 * p1top2;
+                    playersCombined = false;
+                }
+            }
+        }
+
+        if(buttonDualHold < 2)
+        {
+            dualButtonSuccess = false;
+        }
+
+        if (playersAttracting)
+        {
+            players[0].Attract(players[1].transform);
+            players[1].Attract(players[0].transform);
+        }
+    }
+
+    public void buttonPressed()
+    {
+        buttonDualHold++;
+    }
+
+    public void buttonReleased()
+    {
+        buttonDualHold--;
+    }
+
+    public void Merge(GameObject p)
+    {
+        //dualButtonSuccess = false;
+
+        playersAttracting = false;
+        playersCombined = true;
+        Debug.Log("Players should merge: " + p.name);
+
+        if(players[0].GetComponent<FixedJoint>() == null)
+        {
+            joint = players[0].gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = rBodies[1];
+        }
     }
 }
+
