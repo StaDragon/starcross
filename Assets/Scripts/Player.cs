f52c96dbc1bb7e2be772d1c1900da23433e5a7a4
MaxPirotaisWilton <maxpwhirunda@gmail.com>
2022-07-23 14:27:34 +1200

    Uploaded Unity Work into Repo
    I have treated the repo as our Unity project directory. All files are relevant.
    
    If you want to add additional files, make sure it's to the relevant file under /Assets or, if you can't find one that fits, make a new one under /Assets

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Range(1, 2)]
+    public int playerNum = 1;
+
+    private Rigidbody rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 inputVector = new Vector2(Input.GetAxis("Hor_P" + playerNum.ToString()), Input.GetAxis("Ver_P" + playerNum.ToString()));
+        Debug.Log("Input Debug: Movement_Vector = " + inputVector.ToString() + " Button = " + Input.GetButton("Button_P" + playerNum.ToString()));
+
+
+        rBody.AddForce(10 * (Vector3) inputVector - rBody.velocity);
+
+    }
+
+    public Rigidbody getRigidbody()
+    {
+        return rBody;
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public GameObject debugMiddle;
+
+    private Player[] players;
+    private Vector3 playerMiddle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = FindObjectsOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMiddle = (players[0].transform.position + players[1].transform.position) / 2;
+        debugMiddle.transform.position = playerMiddle;
+    }
+}
